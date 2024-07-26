cf2cc9d33f279781fd0d286599d9c585d9ee3af4
vanhcode <tranvanh2k4@gmail.com>
2024-07-26 16:33:57 +0700

    push

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -164,30 +164,6 @@
                     </ul>
                 </li>
 
-                {{-- Quản lí slider --}}
-                <li class="nav-item">
-                    <a href="{{ route('banner.index') }}" class="nav-link">
-                        <i class="nav-icon fas fa-copy"></i>
-                        <p>
-                            {{ trans('language.banner.sidebarTitle') }}
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{ route('banner.index') }}" class="nav-link">
-                                <i class="nav-icon fas fa-columns"></i>
-                                <p>{{ trans('language.banner.list') }}</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('banner.create') }}" class="nav-link">
-                                <i class="nav-icon fas fa-columns"></i>
-                                <p>{{ trans('language.banner.add') }}</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
 
                 {{-- service --}}
                 <li class="nav-item">
