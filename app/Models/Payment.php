5450db398425102238f51f6795081bb5ee3c2c1e
nqtruongg <truong290803@gmail.com>
2024-08-03 08:21:20 +0700

    fix models Payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -13,4 +13,9 @@ class Payment extends Model
     protected $table = 'payments';
 
     protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
